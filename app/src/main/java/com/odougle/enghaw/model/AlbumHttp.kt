d55c5ed6e533d0387a3a74a95988b1be473cdd0e
Douglas Gomes <douglasgommez@gmail.com>
2021-10-08 20:45:05 -0300

    implemented AlbumHttp.kt

---
+++ app/src/main/java/com/odougle/enghaw/model/AlbumHttp.kt
@@ -0,0 +1,29 @@
+package com.odougle.enghaw.model
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.util.concurrent.TimeUnit
+
+object AlbumHttp {
+    const val BASE_URL = "https://raw.githubusercontent.com/nglauber/dominando_android3/master/enghaw/"
+
+    fun loadAlbuns() : Array<Album>? {
+        val client = OkHttpClient.Builder()
+            .readTimeout(5, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .build()
+
+        val request = Request.Builder()
+            .url("${BASE_URL}enghaw.json")
+            .build()
+        return try {
+            val response = client.newCall(request).execute()
+            val json = response.body()?.string()
+            Gson().fromJson(json, Array<Album>::class.java)
+        }catch (e: Exception){
+            e.printStackTrace()
+            null
+        }
+    }
+}
