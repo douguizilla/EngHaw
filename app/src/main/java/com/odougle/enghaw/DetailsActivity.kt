a149ab948da9271abd84089574aa4fdd51007a36
Douglas Gomes <douglasgommez@gmail.com>
2021-10-16 10:57:58 -0300

    Complemented DetailsActivity.kt with missing configurations about TitleBar.

--- app/src/main/java/com/odougle/enghaw/DetailsActivity.kt
+++ app/src/main/java/com/odougle/enghaw/DetailsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.odougle.enghaw.databinding.ActivityDetailsBinding
 import com.odougle.enghaw.model.Album
 import com.odougle.enghaw.model.AlbumHttp
@@ -19,7 +20,7 @@ class DetailsActivity : AppCompatActivity() {
         val album = intent.getParcelableExtra<Album>(EXTRA_ALBUM)
         if(album != null){
             loadCover(album)
-            initTitle(album.title)
+            initTitlebar(album.title)
             fillFields(album)
         }
     }
@@ -30,9 +31,16 @@ class DetailsActivity : AppCompatActivity() {
             .into(binding.imgCover)
     }
 
-    private fun initTitle(title: String) {
+    private fun initTitlebar(title: String) {
         binding.apply {
             setSupportActionBar(toolbar)
+            if(appBar != null){
+                if(appBar?.layoutParams is CoordinatorLayout.LayoutParams){
+                    val lp = appBar?.layoutParams as CoordinatorLayout.LayoutParams
+                    lp.height = resources.displayMetrics.widthPixels
+                }
+            }
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
             if(collapseToolbar != null){
                 supportActionBar?.setDisplayShowTitleEnabled(true)
                 collapseToolbar?.title = title
