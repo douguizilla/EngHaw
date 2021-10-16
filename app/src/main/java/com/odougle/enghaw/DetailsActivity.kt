3ed807af47779f0d3addc06aee1a9196431cad18
Douglas Gomes <douglasgommez@gmail.com>
2021-10-16 10:51:47 -0300

    Implemented DetailsActivity.kt.

--- app/src/main/java/com/odougle/enghaw/DetailsActivity.kt
+++ app/src/main/java/com/odougle/enghaw/DetailsActivity.kt
@@ -1,8 +1,14 @@
 package com.odougle.enghaw
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odougle.enghaw.databinding.ActivityDetailsBinding
+import com.odougle.enghaw.model.Album
+import com.odougle.enghaw.model.AlbumHttp
+import com.squareup.picasso.Picasso
+import java.lang.StringBuilder
 
 class DetailsActivity : AppCompatActivity() {
     private lateinit var binding : ActivityDetailsBinding
@@ -10,5 +16,62 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val album = intent.getParcelableExtra<Album>(EXTRA_ALBUM)
+        if(album != null){
+            loadCover(album)
+            initTitle(album.title)
+            fillFields(album)
+        }
     }
+
+    private fun loadCover(album: Album) {
+        Picasso.get()
+            .load(AlbumHttp.BASE_URL + album.coverBig)
+            .into(binding.imgCover)
+    }
+
+    private fun initTitle(title: String) {
+        binding.apply {
+            setSupportActionBar(toolbar)
+            if(collapseToolbar != null){
+                supportActionBar?.setDisplayShowTitleEnabled(true)
+                collapseToolbar?.title = title
+            }else{
+                supportActionBar?.setDisplayShowTitleEnabled(false)
+            }
+        }
+    }
+
+    private fun fillFields(album: Album) {
+        binding.contentDetails.apply {
+            txtTitle.text = album.title
+            txtYear.text = album.year.toString()
+            txtRecordingCompany.text = album.recordingCompany
+            var sb = StringBuilder()
+            for(member in album.formattion) {
+                if(sb.isNotEmpty()) sb.append('\n')
+                sb.append(member)
+            }
+            txtFormation.text = sb.toString()
+            sb = StringBuilder()
+            album.tracks.forEachIndexed { index, track ->
+                if(sb.isNotEmpty()) sb.append('\n')
+                sb.append(index + 1).append(". ").append(track)
+            }
+            txtSongs.text = sb.toString()
+        }
+    }
+
+    companion object{
+        const val EXTRA_ALBUM = "album"
+        fun start(context: Context, album: Album){
+            context.startActivity(Intent(context, DetailsActivity::class.java).apply{
+                putExtra(EXTRA_ALBUM, album)
+            })
+        }
+    }
+
+
+
+
 }
