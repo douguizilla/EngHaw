00ca3f83fd460948fa3f5482ca1c1f5b913dd6fb
Douglas Gomes <douglasgommez@gmail.com>
2021-10-16 10:37:36 -0300

    created DetailsActivity.kt and its layout. Implemented activity_details.xml layout.

---
+++ app/src/main/java/com/odougle/enghaw/DetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.odougle.enghaw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.odougle.enghaw.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
